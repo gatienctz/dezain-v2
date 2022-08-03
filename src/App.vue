2c26e739b8b9596fa194e6ee152d289defcdee90
Gatien CONTINSOUZAS <45015039+LariXii@users.noreply.github.com>
2022-08-03 02:17:20 +0200

    Display Guesses & solution

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-  <v-toolbar dense elevation="2">
-    <v-toolbar-items>
-      <v-btn-toggle mandatory multiple v-model="selectedAlphabet">
-        <v-btn v-for="alphabet in alphabetsMap" value="alphabet.type"> {{ alphabet.name }}</v-btn>
-      </v-btn-toggle>
-      <v-btn-toggle mandatory multiple v-model="selectedOptions">
-        <v-btn v-for="opt in optionsMap" value="opt.type"> {{ opt.name }}</v-btn>
-      </v-btn-toggle>
-    </v-toolbar-items>
-  </v-toolbar>
-  <p> {{ selectedOptions }}</p>
-  <p> {{ selectedAlphabet }}</p>
-  <kanaItem name="kana.name" symbol="kana.kana" v-for="kana in katakana" :key="kana.name"></kanaItem>
+  <v-app>
+    <v-main>
+      <v-toolbar dense elevation="2">
+        <v-toolbar-items>
+          <v-btn-toggle mandatory multiple v-model="selectedAlphabet">
+            <v-btn x-small v-for="alphabet in alphabetsMap" :value="alphabet.alphabet" :key="alphabet.name"> {{ alphabet.name }}</v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle mandatory multiple v-model="selectedOptions">
+            <v-btn x-small v-for="opt in optionsMap" :value="opt.type" :key="opt.type"> {{ opt.name }}</v-btn>
+          </v-btn-toggle>
+          <v-btn @click="getGuesses">Randomize</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-container>
+        <v-spacer>Symbole à trouver</v-spacer>
+        <v-row :align="center">
+          <v-col>
+            <kanaItem v-if="solution" :name="solution.roumaji" :symbol="solution.kana"></kanaItem>
+          </v-col>
+        </v-row>
+        <v-spacer>Choix</v-spacer>
+        <v-row :align="center">
+          <v-col v-for="guess in guesses" :key="guess.roumaji">
+            <kanaItem :name="guess.roumaji" :symbol="guess.kana"></kanaItem>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+//import HelloWorld from './components/HelloWorld.vue'
 import kanaItem from './components/kana-item.vue'
 import katakana from './resource/js/katakana.json'
 import hiragana from './resource/js/hiragana.json'
 
 export default {
   name: 'App',
-  vuetify,
   components: {
-    HelloWorld,
     kanaItem
   },
   setup() {
-    const KATAKANA = "Katakana";
-    const HIRAGANA = "Hiragana";
 
     const optionsMap = [
       { name: 'Handakuten', type: 'handakuon' },
@@ -39,25 +50,34 @@ export default {
       { name: 'Gojūon', type: 'gojuuon' },
       { name: 'Yōon', type: 'youon' }
     ]
-    const alphabetsMap = [KATAKANA, HIRAGANA];
+    const alphabetsMap = [
+      { name: "Katakana", alphabet: require('./resource/js/katakana.json') },
+      { name: "Hiragana", alphabet: require('./resource/js/hiragana.json') },
+    ];
 
-    const selectedAlphabet = ['Katakana'];
-    const selectedOptions = ['Gojūon'];
+    const selectedAlphabet = ref();
+    const selectedOptions = ref();
+
+    let alphabet;
+    const solution = ref();
+    const guesses = ref();
+
+    const getGuesses = () => {
+      console.log("Get Guesses called");
+      alphabet = setupAlphabet(selectedAlphabet.value, selectedOptions.value);
+      console.log("Alphabet : " + alphabet)
+      solution.value = getRandomItem(alphabet);
+      console.log("Solution : " + solution.value);
+      guesses.value = getRandomAnswers(alphabet, solution.value, 5);
+      console.log("Guesses : " + guesses.value);
+    }
 
     const setupAlphabet = (selectedAlphabet, selectedOptions) => {
       var retAlphabet = null;
-      if (selectedAlphabet.length == 1) {
-        if (KATAKANA === selectedAlphabet[0]) {
-          retAlphabet = katakana;
-        }
-        else if (HIRAGANA === selectedAlphabet[0]) {
-          retAlphabet = hiragana;
-        }
-      }
-      else if (selectedAlphabet.length == 2) {
-        retAlphabet = katakana.concat(hiragana);
-      }
-      return retAlphabet.filter(item => optionsMap.filter(opt => selectedOptions.includes(opt.name)).map(obj => obj.type).includes(item.type))
+      console.log(selectedAlphabet);
+      retAlphabet = selectedAlphabet.flat();
+      console.log(retAlphabet);
+      return retAlphabet.filter(item => selectedOptions.includes(item.type));
     }
 
     const randomIndex = (array) => {
@@ -77,8 +97,8 @@ export default {
 
     const getRandomAnswers = (array, solution, possibleAnswer = 3) => {
       const answers = [solution];
-      possibleAnswer--;//Decrease nb of possible answer because the solution is part of it
-      for (i = 0; i < possibleAnswer; i++) {
+      possibleAnswer--;
+      for (let i = 0; i < possibleAnswer; i++) {
         var item;
         do {
           item = getRandomItem(array);
@@ -90,11 +110,30 @@ export default {
       return answers;
     }
 
+    onMounted(() => {
+      console.log("OnMounted -- Init :");
+      selectedAlphabet.value = [alphabetsMap[0].alphabet];
+      console.log("Selected Alphabet : " + selectedAlphabet.value);
+      selectedOptions.value = [optionsMap[0].type];
+      console.log("Selected Options : " + selectedOptions.value);
+      getGuesses();
+    })
+
     return {
-      options,
-      getKataKanaByRoumaji,
-      getHiraganaByRoumaji,
-      selectedAlphabet
+      optionsMap,
+      alphabetsMap,
+      selectedAlphabet,
+      selectedOptions,
+      setupAlphabet,
+      getRandomAnswers,
+      getRandomItem,
+      randomIndex,
+      katakana,
+      hiragana,
+      alphabet,
+      solution,
+      guesses,
+      getGuesses
     }
   }
 }
--- src/components/kana-item.vue
+++ src/components/kana-item.vue
@@ -1,4 +1,15 @@
 <template>
+<v-btn>
     <div>{{ symbol }}</div>
     <div>{{ name }}</div>
+</v-btn>
 </template>
+
+<script>
+    export default {
+        props : {
+            symbol: { type: String, default: null },
+            name: { type: String, default: null }
+        }
+    }
+</script>
