2c380f442071e3936739d2c3eedc2ddfa049503e
Gatien CONTINSOUZAS <45015039+LariXii@users.noreply.github.com>
2022-08-03 19:23:18 +0200

    Change kanaItem to v-card

--- src/components/kana-item.vue
+++ src/components/kana-item.vue
@@ -1,16 +1,16 @@
 <template>
-<v-btn>
-    <div v-if="isSymbol">{{ symbol }}</div>
-    <div v-if="!isSymbol">{{ name }}</div>
-</v-btn>
+    <v-card elevation="24" outlined tile>
+        <v-card-title v-if="isSymbol">{{ symbol }}</v-card-title>
+        <v-card-title v-if="!isSymbol">{{ name }}</v-card-title>
+    </v-card>
 </template>
 
 <script>
-    export default {
-        props : {
-            symbol: { type: String, default: null },
-            name: { type: String, default: null },
-            isSymbol: { type: Boolean, default: null },
-        }
+export default {
+    props: {
+        symbol: { type: String, default: null },
+        name: { type: String, default: null },
+        isSymbol: { type: Boolean, default: null },
     }
+}
 </script>
